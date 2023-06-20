27047840ead4478d59dba8d70871850d36a32063
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-06-20 23:00:34 +0530

    added basic template for theme

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+add_theme_support( 'custom-logo');
+add_theme_support( 'title-tag' );
+
+function laslesvpn_enqueue_styles() {
+    wp_enqueue_style( 'font-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
+
+}
+add_action( 'wp_enqueue_scripts', 'laslesvpn_enqueue_styles' );
+?>
